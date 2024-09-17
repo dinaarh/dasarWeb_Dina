e337c7290065c41bab0d18d7a8f7d8a9701de66b
dinaarh <rahmawatidina0404@gmail.com>
2024-09-17 10:42:15 +0700

    P4_Praktikum 3_langkah 5 _Penggunaan Operator PHP_operator pembanding_Soal no 3.2

--- dasarWeb_Dina/P4_PHP/operator.php
+++ dasarWeb_Dina/P4_PHP/operator.php
@@ -9,7 +9,7 @@ $hasilBagi = $a / $b;
 $sisaBagi = $a % $b;
 $pangkat = $a ** $b;
 
-echo "Hasil dari perhitungan a : $a dan  b : $b <br><br>";
+echo "Hasil dari perhitungan operator Aritmatika & Pemangkatan a : $a dan  b : $b <br><br>";
 echo "Hasil Tambah : $hasilTambah <br>";
 echo "Hasil Kurang : $hasilKurang <br>";
 echo "Hasil Kali : $hasilKali <br>";
@@ -17,4 +17,18 @@ echo "Hasil Bagi : $hasilBagi <br>";
 echo "Hasil Sisa Bagi : $sisaBagi <br>";
 echo "Hasil Pangkat : $pangkat <br>";
 
+$hasilSama = $a == $b;
+$hasilTidakSama = $a != $b;
+$hasilLebihKecil = $a < $b;
+$hasilLebihBesar = $a > $b;
+$hasilLebihKecilSama = $a <= $b;
+$hasilLebihBesarSama= $a >= $b;
+
+echo "<br><br>Hasil dari perhitungan operator pembanding a : $a dan  b : $b <br><br>";
+echo "Hasil Sama : $hasilSama <br>";
+echo "Hasil Tidak Sama : $hasilTidakSama <br>";
+echo "Hasil Lebih Kecil : $hasilLebihKecil <br>";
+echo "Hasil Lebih Besar : $hasilLebihBesar <br>";
+echo "Hasil Lebih Kecil Sama : $hasilLebihKecilSama <br>";
+echo "Hasil Lebih Besar Sama : $hasilLebihBesarSama <br>";
 ?>
