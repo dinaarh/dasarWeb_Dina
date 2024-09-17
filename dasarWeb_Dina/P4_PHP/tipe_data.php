50a07103846e24ce25489669f0d0afbfa388d873
dinaarh <rahmawatidina0404@gmail.com>
2024-09-17 09:43:36 +0700

    P4_Praktikum 2_langkah 11 _Penggunaan Tipe Data_tambah kode tipe data String

--- dasarWeb_Dina/P4_PHP/tipe_data.php
+++ dasarWeb_Dina/P4_PHP/tipe_data.php
@@ -34,4 +34,16 @@ $apakahSiswaSudahUjian = false;
 var_dump($apakahSiswaLulus);
 echo "<br>";
 var_dump($apakahSiswaSudahUjian);
+
+echo "<br>";
+$namaDepan = "Ibnu";
+$namaBelakang = 'Jakaria';
+
+$namaLengkap = "{$namaDepan} {$namaBelakang}";
+$namaLengkap2 = $namaDepan . ' ' . $namaBelakang;
+
+echo "Nama Depan: {$namaDepan} <br>";
+echo 'Nama Belakang: ' . $namaBelakang . '<br>';
+
+echo $namaLengkap;
 ?>
