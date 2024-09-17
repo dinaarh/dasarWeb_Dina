e3de0164037ca67f7e8b280b285a946b878ece4e
dinaarh <rahmawatidina0404@gmail.com>
2024-09-17 09:28:35 +0700

    P4_Praktikum 2_langkah 8 _Penggunaan Tipe Data_tambah kode tipe data boolean

--- dasarWeb_Dina/P4_PHP/tipe_data.php
+++ dasarWeb_Dina/P4_PHP/tipe_data.php
@@ -26,4 +26,12 @@ echo "Bahasa Indonesia: {$nilaiBahasaIndonesia} <br>";
 echo "Rata-rata: {$rataRata} <br>";
 
 var_dump($rataRata);
+
+echo "<br>";
+$apakahSiswaLulus = true;
+$apakahSiswaSudahUjian = false;
+
+var_dump($apakahSiswaLulus);
+echo "<br>";
+var_dump($apakahSiswaSudahUjian);
 ?>
