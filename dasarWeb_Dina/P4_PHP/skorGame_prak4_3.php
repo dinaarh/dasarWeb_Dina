303809e67faeebb612d180ee9fdaef7f1b4b3a0a
dinaarh <rahmawatidina0404@gmail.com>
2024-09-19 22:03:07 +0700

    P4_Praktikum 4_langkah 25_dgn ternary_Soal skor_Soal no 4.8

--- dasarWeb_Dina/P4_PHP/skorGame_prak4_3.php
+++ dasarWeb_Dina/P4_PHP/skorGame_prak4_3.php
@@ -1,16 +1,19 @@
 <?php
 // Poin yang dikumpulkan pemain
 $poin_pemain = 550;
+$hadiah = ($poin_pemain > 500) ? "YA" : "TIDAK";
+
+echo "===================SKOR================<br>";
+echo "Total skor pemain adalah: " . $poin_pemain . "<br>";
+echo "Apakah pemain mendapatkan hadiah tambahan? $hadiah";
+
 
 // Batas poin
-$batas_hadiah = 500;
-
-echo "Total skor pemain adalah: " . $poin_pemain . "<br>";
-
+// $batas_hadiah = 500;
 // Struktur kontrol untuk menentukan apakah pemain mendapatkan hadiah tambahan
-if ($poin_pemain > $batas_hadiah) {
-    echo "Apakah pemain mendapatkan hadiah tambahan? YA";
-} else {
-    echo "Apakah pemain mendapatkan hadiah tambahan? TIDAK";
-}
+// if ($poin_pemain > $batas_hadiah) {
+//     echo "Apakah pemain mendapatkan hadiah tambahan? YA";
+// } else {
+//     echo "Apakah pemain mendapatkan hadiah tambahan? TIDAK";
+// }
 ?>
