2983cc2ff270aba64d5fd775dc28ade081390596
dinaarh <rahmawatidina0404@gmail.com>
2024-09-19 21:33:14 +0700

    P4_Praktikum 4_langkah 23_Penggunaan Struktur Kontrol PHP_Soal cerita diskon_Soal no 4.7

---
+++ dasarWeb_Dina/P4_PHP/hargaDiskon_prak4_2.php
@@ -0,0 +1,25 @@
+<?php
+$harga_produk = 120000;
+$diskon_persen = 20;
+$batas_diskon = 100000;
+$harga_akhir = 0;
+
+echo "<center>Harga awal produk : Rp " . number_format($harga_produk, 0, ',', '.') . "</center><br>";
+
+// Struktur kontrol untuk mengecek apakah harga memenuhi syarat diskon
+if ($harga_produk > $batas_diskon) {
+    // Hitung diskon
+    $diskon = ($diskon_persen / 100) * $harga_produk;
+    
+    // Hitung harga setelah diskon
+    $harga_akhir = $harga_produk - $diskon;
+    echo "<center>Mendapat diskon: Rp " . number_format($diskon, 0, ',', '.') . " ( ". $diskon_persen . "% ) </center><br>";
+    echo "<center>====================================</center><br>";
+    echo "<center>Harga yang harus dibayar setelah diskon: </center><br><center>Rp " . number_format($harga_akhir, 0, ',', '.') . "</center>";
+} else {
+    // tidak memenuhi syarat diskon, harga tetap
+    $harga_akhir = $harga_produk;
+    echo "Tidak mendapat diskon, harga tetap: Rp " . number_format($harga_produk, 0, ',', '.');
+}
+
+?>
