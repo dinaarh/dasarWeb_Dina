68431a17c4ea7b15e79f0ce80f78effb484e91cf
dinaarh <rahmawatidina0404@gmail.com>
2024-09-24 10:36:31 +0700

    tambah folder

---
+++ dasarWeb_Dina/P5/PHP2/fungsi.php
@@ -0,0 +1,20 @@
+<?php
+//membuat fungsi
+function perkenalan($nama, $salam){
+    echo $salam .", ";
+    echo "Perkenalkan, nama saya ". $nama . "<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+// memanggil fungsi yang sudah dibuat
+perkenalan("Dinaa", "Hallo");
+
+echo "<hr>";
+
+$saya = "Dinaa";
+$ucapanSalam = "Selamat Pagi";
+
+//memanggil lagi
+perkenalan($saya, $ucapanSalam);
+
+?>
