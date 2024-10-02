ea8ab8ba422b968986543dcbe9a428f43d4a2a6c
dinaarh <rahmawatidina0404@gmail.com>
2024-10-02 15:37:17 +0700

    P6_PHP2_langkah 1_Fungsi_soal 4

---
+++ dasarWeb_Dina/P6_PHP2_array/fungsi.php
@@ -0,0 +1,12 @@
+<?php
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Dina rahmawati <br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+//memanggil fungsi yang sudah dibuat
+perkenalan();
+perkenalan();
+
+?>
