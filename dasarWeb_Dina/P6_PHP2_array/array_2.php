368b65f6ac8747d5a56aa7bf51a51aa01724070a
dinaarh <rahmawatidina0404@gmail.com>
2024-10-02 14:05:22 +0700

    P6_PHP2_praktikum 2_langkah 3_Associative Array_soal 2

--- dasarWeb_Dina/P6_PHP2_array/array_2.php
+++ dasarWeb_Dina/P6_PHP2_array/array_2.php
@@ -5,6 +5,45 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Associative array</title>
+    <style>
+    body {
+        font-family: Arial, sans-serif;
+    }
+
+    table {
+        border-collapse: collapse;
+        width: 50%;
+        margin: 20px auto;
+        font-size: 18px;
+    }
+
+    table,
+    th,
+    td {
+        border: 1px solid #dddddd;
+    }
+
+    th,
+    td {
+        padding: 12px;
+        text-align: left;
+    }
+
+    th {
+        background-color: #f2f2f2;
+    }
+
+    tr:hover {
+        background-color: #f1f1f1;
+    }
+
+    caption {
+        caption-side: top;
+        font-size: 22px;
+        font-weight: bold;
+        margin-bottom: 10px;
+    }
+    </style>
 </head>
 
 <body>
@@ -13,11 +52,23 @@
             'nama' => "Elok Nur Hamdana", 
             'domisili' => "Malang", 
             'jenis_kelamin' => "Perempuan"];
-
-        echo "Nama : {$Dosen ['nama']} <br>";
-        echo "Domisili : {$Dosen ['domisili']} <br>";
-        echo "Jenis Kelamin : {$Dosen ['jenis_kelamin']} <br>";
     ?>
+
+    <table>
+        <caption>Informasi Dosen</caption>
+        <tr>
+            <th>Nama</th>
+            <td><?php echo $Dosen['nama']; ?></td>
+        </tr>
+        <tr>
+            <th>Domisili</th>
+            <td><?php echo $Dosen['domisili']; ?></td>
+        </tr>
+        <tr>
+            <th>Jenis Kelamin</th>
+            <td><?php echo $Dosen['jenis_kelamin']; ?></td>
+        </tr>
+    </table>
 </body>
 
 </html>
