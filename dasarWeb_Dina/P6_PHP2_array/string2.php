cc262697e2a7a7471c2e1dd824d5962e5ed61c8d
dinaarh <rahmawatidina0404@gmail.com>
2024-10-02 16:55:30 +0700

    P6_PHP2_langkah 3_String-Escape Character_soal 14

---
+++ dasarWeb_Dina/P6_PHP2_array/string2.php
@@ -0,0 +1,12 @@
+<?php
+echo "Baris\nbaru <br>" ; //soal 14.a
+echo 'Baris\nbaru <br>' ; //soal 14.b
+echo "Halo\rDunia <br>" ; //soal 14.c
+echo 'Halo\rDunia <br>' ; //soal 14.d
+
+echo "<pre>Halo\tDunia!</pre>"; //soal 14.e
+echo '<pre>Halo\tDunia!</pre>'; //soal 14.f
+
+echo "Katakanlah \"Tidak pada narkoba!\" <br>"; //soal 14.g
+echo 'Katakanlah \'Tidak pada narkoba!\' <br>'; //soal 14.h
+?>
