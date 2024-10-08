019e81b397edfe0cec70b28224e76c10b1601315
dinaarh <rahmawatidina0404@gmail.com>
2024-10-08 07:49:00 +0700

    P7_PHP-Form_Praktikum1_Function Isset_langkah 5_soal 1.2

--- dasarWeb_Dina/P7_PHP_Form/isset.php
+++ dasarWeb_Dina/P7_PHP_Form/isset.php
@@ -5,4 +5,11 @@ if(isset($umur) && $umur >= 18) {
 } else {
     echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan";
 }
+
+$data= array("nama" => "Jane", "usia" => 25);
+if(isset($data["nama"])) {
+    echo "<br>Nama: " . $data["nama"];
+} else {
+    echo "<br>Variabel 'nama' tidak ditemukan dalam array.";
+}
 ?>
