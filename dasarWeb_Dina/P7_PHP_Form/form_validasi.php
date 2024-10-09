14a73ede037536d913fb9d404f599b7f85ac6a62
dinaarh <rahmawatidina0404@gmail.com>
2024-10-09 21:55:30 +0700

    P7_PHP-Form_Praktikum 7_Validasi Form-ajax_langkah 9_soal 7.3

--- dasarWeb_Dina/P7_PHP_Form/form_validasi.php
+++ dasarWeb_Dina/P7_PHP_Form/form_validasi.php
@@ -2,13 +2,13 @@
 <html>
 
 <head>
-    <title>Form Input dengan Validasi</title>
+    <title>Form Input dengan Validasi - AJAX</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
 <body>
-    <h1>Form Input dengan Validasi</h1>
-    <form id="myForm" method="post" action="proses_validasi.php">
+    <h1>Form Input dengan Validasi - AJAX</h1>
+    <form id="myForm">
         <label for="nama">Nama: </label>
         <input type="text" id="nama" name="nama">
         <span id="nama-error" style="color: red;"></span><br><br>
@@ -20,9 +20,12 @@
         <input type="submit" value="Submit">
     </form>
 
+    <div id="hasil"></div>
+
     <script>
     $(document).ready(function() {
         $("#myForm").submit(function(event) {
+            event.preventDefault();
             var nama = $("#nama").val();
             var email = $("#email").val();
             var valid = true;
@@ -38,9 +41,21 @@
             } else {
                 $("#email-error").text("");
             }
-            if (!valid) {
-                event.preventDefault();
-                //Menghentikan pengiriman form jika validasi gagal
+            if (valid) {
+                $.ajax({
+                    url: "proses_validasi.php",
+                    type: "POST",
+                    data: {
+                        nama: nama,
+                        email: email
+                    },
+                    success: function(response) {
+                        $("#hasil").html(response);
+                    },
+                    error: function() {
+                        $("#hasil").html("Terjadi kesalahan dalam pengiriman data.");
+                    }
+                });
             }
         });
     });
