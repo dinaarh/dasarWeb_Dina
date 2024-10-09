cd3c7bb08b0d6c7ca0ac50253a6d6e34165ab2e2
dinaarh <rahmawatidina0404@gmail.com>
2024-10-09 17:52:55 +0700

    P7_PHP-Form_Praktikum 4_HTML Injection_langkah 6-7_soal 4.2

--- dasarWeb_Dina/P7_PHP_Form/html_aman.php
+++ dasarWeb_Dina/P7_PHP_Form/html_aman.php
@@ -3,6 +3,11 @@
 
 <head>
     <title>HTML Injection</title>
+    <style>
+    .error {
+        color: red;
+    }
+    </style>
 </head>
 
 <body>
@@ -10,7 +15,9 @@
 
     <?php 
     $inputErr = "";
+    $emailErr = "";
     $input = "";
+    $email = "";
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         
@@ -19,16 +26,34 @@
         } else {
             $input = $_POST['input'];
             $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+        }
 
-            echo "<p>Hasil input: $input</p>";
+        if (empty($_POST["email"])) {
+            $emailErr = "Email harus diisi!";
+        } else {
+            $email = $_POST['email'];
+            $email = htmlspecialchars($email, ENT_QUOTES, 'UTF-8');
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                // Lanjutkan dengan pengolahan email yang aman
+                echo "<p>Data berhasil diterima!</p>";
+                echo "<p>Hasil input: $input</p>";
+                echo "<p>Email: $email</p>";
+            } else {
+                // Tangani input yang tidak valid
+                $emailErr = "Format email tidak valid.";
+            }
         }
     }
     ?>
 
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-        <label for="input">Inputkan data: </label>
+        <label for="input">Inputkan data: </label><br>
         <input type="text" name="input" id="input" value="<?php echo $input; ?>" required>
         <span class="error"><?php echo $inputErr; ?></span><br><br>
+
+        <label for="email">Inputkan email: </label><br>
+        <input type="text" name="email" id="email" value="<?php echo $email; ?>" required>
+        <span class="error"><?php echo $emailErr; ?></span><br><br>
 
         <input type="submit" name="submit" value="Submit">
     </form>
