b44d54c79fa44ae8fd73589dd2092eac4a181a7e
dinaarh <rahmawatidina0404@gmail.com>
2024-10-09 16:57:42 +0700

    P7_PHP-Form_Praktikum 4_HTML Injection_langkah 3_soal 4.1

--- dasarWeb_Dina/P7_PHP_Form/html_aman.php
+++ dasarWeb_Dina/P7_PHP_Form/html_aman.php
@@ -1,4 +1,37 @@
-<?php 
-$input = $_POST['input'];
-$input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
-?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>HTML Injection</title>
+</head>
+
+<body>
+    <h2 style="text-align: center;">Form Input</h2>
+
+    <?php 
+    $inputErr = "";
+    $input = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        
+        if (empty($_POST["input"])){
+            $inputErr = "Inputkan data dahulu!";
+        } else {
+            $input = $_POST['input'];
+            $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
+
+            echo "<p>Hasil input: $input</p>";
+        }
+    }
+    ?>
+
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <label for="input">Inputkan data: </label>
+        <input type="text" name="input" id="input" value="<?php echo $input; ?>" required>
+        <span class="error"><?php echo $inputErr; ?></span><br><br>
+
+        <input type="submit" name="submit" value="Submit">
+    </form>
+</body>
+
+</html>
