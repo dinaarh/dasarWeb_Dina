f028fc9fc5851197a15582221924c8bea0156adc
dinaarh <rahmawatidina0404@gmail.com>
2024-10-09 18:13:49 +0700

    P7_PHP-Form_Praktikum 5_Penggunaan Regex Pada PHP_langkah 6_soal 5.2

--- dasarWeb_Dina/P7_PHP_Form/regex.php
+++ dasarWeb_Dina/P7_PHP_Form/regex.php
@@ -6,4 +6,13 @@
     } else {
         echo "Tidak ada huruf kecil!";
     }
+
+    echo"<br><hr>";
+    $pattern = '/[0-9]+/'; // Cocokkan satu atau lebih digit.
+    $text = 'There are 123 apples.';
+    if (preg_match($pattern, $text, $matches)) {
+        echo "Cocokkan: " . $matches[0];
+    } else {
+        echo "Tidak ada yang cocok!";
+    }
 ?>
