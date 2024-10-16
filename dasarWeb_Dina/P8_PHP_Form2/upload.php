f6b53033e22dc21c569684f308458da1341cb4d1
dinaarh <rahmawatidina0404@gmail.com>
2024-10-16 07:54:26 +0700

    P8_PHP-Form2_Praktikum 1_Upload File-menampilkan gambar_langkah 9_soal 5

--- dasarWeb_Dina/P8_PHP_Form2/upload.php
+++ dasarWeb_Dina/P8_PHP_Form2/upload.php
@@ -9,7 +9,10 @@ if(isset($_POST["submit"])) {
     
     if(in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
         if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)){
-            echo "File berhasil diunggah.";
+            echo "File berhasil diunggah.<br>";
+
+            // soal 5 gambar lebar 200 & tinggi otomatis
+            echo "<img src='" . $targetfile . "' width='200' alt='Thumbnail' style='height:auto;'>";
         } else {
             echo "Gagal mengunggah file.";
         }
