a9dc61977224cf1be47675cc1ec7786b90ade142
dinaarh <rahmawatidina0404@gmail.com>
2024-10-16 08:25:22 +0700

    P8_PHP-Form2_Praktikum 1_Upload File-lebih dari_langkah 9_soal 6

--- dasarWeb_Dina/P8_PHP_Form2/upload.php
+++ dasarWeb_Dina/P8_PHP_Form2/upload.php
@@ -4,15 +4,17 @@ if(isset($_POST["submit"])) {
     $targetfile = $targetdir. basename($_FILES["myfile"]["name"]);
     $fileType =strtolower(pathinfo($targetfile, PATHINFO_EXTENSION)); 
 
-    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
-    $maxsize = 5*1024*1024;
+    // $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    // $maxsize = 5*1024*1024;
+    $allowedExtensions = array("txt", "pdf", "doc", "docx");
+    $maxsize = 3*1024*1024; 
     
     if(in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
         if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)){
             echo "File berhasil diunggah.<br>";
 
             // soal 5 gambar lebar 200 & tinggi otomatis
-            echo "<img src='" . $targetfile . "' width='200' alt='Thumbnail' style='height:auto;'>";
+            // echo "<img src='" . $targetfile . "' width='200' alt='Thumbnail' style='height:auto;'>";
         } else {
             echo "Gagal mengunggah file.";
         }
