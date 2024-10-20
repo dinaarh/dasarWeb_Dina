74b18ab75b211933ea3519d88b89a37584028171
dinaarh <rahmawatidina0404@gmail.com>
2024-10-20 08:34:15 +0700

    P8_PHP-Form2_Praktikum 3_Upload File dengan PHP dan Jquery_langkah 6_soal 11

--- dasarWeb_Dina/P8_PHP_Form2/form_upload_ajax.php
+++ dasarWeb_Dina/P8_PHP_Form2/form_upload_ajax.php
@@ -2,12 +2,12 @@
 <html>
 
 <head>
-    <title>Unggah File Dokumen</title>
+    <title>Unggah File Gambar</title>
 </head>
 
 <body>
     <form id="upload-form" action="upload_ajax.php" method="POST" enctype="multipart/form-data">
-        <input type="file" name="file" id="file">
+        <input type="file" name="files[]" id="file" multiple accept=".jpg, .jpeg, .png, .gif">
         <input type="submit" name="submit" value="Unggah">
     </form>
     <div id="status"></div>
--- dasarWeb_Dina/P8_PHP_Form2/upload_ajax.php
+++ dasarWeb_Dina/P8_PHP_Form2/upload_ajax.php
@@ -1,26 +1,32 @@
 <?php
-if (isset($_FILES['file'])) {
+if (isset($_FILES['files'])) {
     $errors = array();
-    $file_name = $_FILES['file']['name'];
-    $file_size = $_FILES['file']['size'];
-    $file_tmp = $_FILES['file']['tmp_name'];
-    $file_type = $_FILES['file']['type'];
-    @$file_ext = strtolower("". end(explode('.', $_FILES['file']['name'])) . "");
-    $extensions = array("pdf", "doc", "docx", "txt");
+    $extensions = array("jpg", "jpeg", "png", "gif");
 
-    if (in_array($file_ext, $extensions) === false) {
-        $errors[] = "Ekstensi file yang diizinkan adalah PDF, DOC, DOCX, ATAU TXT.";
+    foreach ($_FILES['files']['tmp_name'] as $key => $tmp_name) {
+        $file_name = $_FILES['files']['name'][$key];
+        $file_size = $_FILES['files']['size'][$key];
+        $file_tmp = $_FILES['files']['tmp_name'][$key];
+        $file_type = $_FILES['files']['type'][$key];
+        $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
+
+        if (in_array($file_ext, $extensions) === false) {
+            $errors[] = "Ekstensi file yang diizinkan adalah JPG, JPEG, PNG, atau GIF.";
+        }
+
+        if ($file_size > 2097152) {
+            $errors[] = 'Ukuran file tidak boleh lebih dari 2 MB';
+        }
+
+        if (empty($errors)) {
+            move_uploaded_file($file_tmp, "images/" . $file_name);
+        }
     }
-
-    if ($file_size > 2097152) {
-        $errors[] = 'Ukuran file tidak boleh lebih dari 2 MB';
-    }
-
-    if (empty($errors) == true) {
-        move_uploaded_file($file_tmp, "documents/" . $file_name);
-        echo "File berhasil diunggah.";
+    
+    if (!empty($errors)) {
+        echo implode("<br>", $errors);
     } else {
-        echo implode(" ", $errors);
+        echo "Semua file berhasil diunggah.";
     }
 }
 ?>
