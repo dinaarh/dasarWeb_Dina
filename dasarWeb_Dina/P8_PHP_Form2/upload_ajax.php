ba2e7a6bbdf5d12709db39e77c3f9e5ff8ffef75
dinaarh <rahmawatidina0404@gmail.com>
2024-10-21 05:21:01 +0700

    P8_PHP-Form2_Praktikum 4_Menghias Upload File_langkah 4_soal 12

--- dasarWeb_Dina/P8_PHP_Form2/upload_ajax.php
+++ dasarWeb_Dina/P8_PHP_Form2/upload_ajax.php
@@ -1,15 +1,13 @@
 <?php
-if (isset($_FILES['files'])) {
+if (isset($_FILES['file']['name'])) {
     $errors = array();
+    $file_name = $_FILES['file']['name'];
+    $file_size = $_FILES['file']['size'];
+    $file_tmp = $_FILES['file']['tmp_name'];
+    $file_type = $_FILES['file']['type'];
+    $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
     $extensions = array("jpg", "jpeg", "png", "gif");
-
-    foreach ($_FILES['files']['tmp_name'] as $key => $tmp_name) {
-        $file_name = $_FILES['files']['name'][$key];
-        $file_size = $_FILES['files']['size'][$key];
-        $file_tmp = $_FILES['files']['tmp_name'][$key];
-        $file_type = $_FILES['files']['type'][$key];
-        $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
-
+    
         if (in_array($file_ext, $extensions) === false) {
             $errors[] = "Ekstensi file yang diizinkan adalah JPG, JPEG, PNG, atau GIF.";
         }
@@ -18,15 +16,14 @@ if (isset($_FILES['files'])) {
             $errors[] = 'Ukuran file tidak boleh lebih dari 2 MB';
         }
 
-        if (empty($errors)) {
+        if (empty($errors) == true) {
+            if (!file_exists('images')) {
+                mkdir('images', 0777, true);
+            }
             move_uploaded_file($file_tmp, "images/" . $file_name);
+            echo "File berhasil diunggah.";
+        } else {
+            echo implode(" ", $errors);
         }
-    }
-    
-    if (!empty($errors)) {
-        echo implode("<br>", $errors);
-    } else {
-        echo "Semua file berhasil diunggah.";
-    }
 }
 ?>
